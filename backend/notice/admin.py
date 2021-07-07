590a8d40ec5753c38fded6d9291eedc7da8a2b8e
byeongjulee222 <dave.juya777@gmail.com>
2021-07-07 18:38:30 +0900

    210707 | modeling_finished

--- backend/notice/admin.py
+++ backend/notice/admin.py
@@ -2,11 +2,5 @@ from django.contrib import admin
 from .models import Notice
 
 # Register your models here.
-def get_fields_model(model):
-    return [field.name for field in model._meta.get_fields()]
 
-class NoticeAdmin(admin.ModelAdmin):
-    list_display = get_fields_model(Notice)
-    # list_display = ('pk', 'title', 'content', 'public', 'hits', 'top_fixed', 'created_at', 'updated_at')
-
-admin.site.register(Notice, NoticeAdmin)
+admin.site.register(Notice)
