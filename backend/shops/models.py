80ecb5a81347ddee9d9f46a0586c63571aa38b6b
byeongjulee222 <dave.juya777@gmail.com>
2021-07-09 16:31:27 +0900

    210709 | remodeling

--- backend/shops/models.py
+++ backend/shops/models.py
@@ -5,23 +5,23 @@ from django.conf import settings
 # Create your models here.
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=10)
-    marker_path = models.CharField(max_length=100)
+    category_name = models.CharField(max_length=10, blank=True)
+    marker_path = models.CharField(max_length=100, blank=True)
     like_count = models.PositiveIntegerField(default=0)
 
 
 class AccountShopkeeper(models.Model):
-    shopkeeper_name = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=20)
+    shopkeeper_name = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
     
 
 class Coupon(models.Model):
-    coupon_content = models.TextField()
+    coupon_content = models.TextField(blank=True)
     expire_date = models.DateTimeField()
 
 
 class ShopArea(models.Model):
-    area_name = models.CharField(max_length=10)
+    area_name = models.CharField(max_length=10, blank=True)
 
 
 class Shop(models.Model):
@@ -29,24 +29,24 @@ class Shop(models.Model):
     coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     area = models.ForeignKey(ShopArea, on_delete=models.CASCADE, null=True) # 지역 구분용(그룹)
-    shop_name = models.CharField(max_length=20)
-    shop_address = models.CharField(max_length=50)
-    shop_description = models.TextField(null=True)
-    phone_number = models.CharField(max_length=15)
-    open_time = models.TextField()
+    shop_name = models.CharField(max_length=20, blank=True)
+    shop_address = models.CharField(max_length=50, blank=True)
+    shop_description = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    open_time = models.TextField(blank=True)
     like_count = models.PositiveIntegerField(default=0)
-    shop_info_url = models.TextField() 
+    shop_info_url = models.TextField(blank=True) 
     star_score = models.IntegerField() # 추후 고객 평점 데이터
     kakao_score = models.IntegerField(default=0)
     kakao_score_count = models.IntegerField(default=0)
     kakao_review_count = models.IntegerField(default=0)
-    price_range = models.CharField(max_length=15)
-    latitude = models.TextField() # 위도
-    longitude = models.TextField() # 경도
+    price_range = models.CharField(max_length=15, blank=True)
+    latitude = models.TextField(blank=True) # 위도
+    longitude = models.TextField(blank=True) # 경도
     is_subscribe = models.BooleanField(default=False)
     subscribe_time = models.DateField()
     is_new_opend = models.BooleanField(default=False)
-    business_reg_img = models.TextField()
+    business_reg_img = models.TextField(blank=True)
 
     def __str__(self):
         return self.shop_name
@@ -54,9 +54,9 @@ class Shop(models.Model):
 
 class Menu(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True)
-    menu_name = models.CharField(max_length=20)
+    menu_name = models.CharField(max_length=20, blank=True)
     price = models.PositiveIntegerField(blank=True)
-    is_representative = models.BooleanField() # 대표메뉴 여부
+    is_representative = models.BooleanField(default=False) # 대표메뉴 여부
 
 
 class Ingredient(models.Model):
@@ -84,16 +84,16 @@ class Review(models.Model):
     score_cleanliness = models.PositiveIntegerField(default=0)
     score_vibe = models.PositiveIntegerField(default=0)
     score_price = models.PositiveIntegerField(default=0)
-    content = models.TextField()
+    content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class ThemeKeyword(models.Model):
     shop = models.ForeignKey(Shop, on_delete=CASCADE, null=True)
-    theme_keyword = models.CharField(max_length=10)
+    theme_keyword = models.CharField(max_length=10, blank=True)
 
 
 class ShopImage(models.Model):
     shop = models.ForeignKey(Shop, on_delete=CASCADE, null=True)
-    img_url = models.TextField()
+    img_url = models.TextField(blank=True)
