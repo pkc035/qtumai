65451afce3013352c2186fe0902932b7df05957c
ttakku-park <ttakku.park@gmail.com>
2021-07-09 17:36:09 +0900

    chore : view import

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
-
+from .views import KakaoLogInView, GoogleLoginView
 router = DefaultRouter(trailing_slash=True)
 router.register(r"accounts", views.UserViewSet, basename="accounts")
 
