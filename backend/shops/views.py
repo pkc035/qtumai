252ce7b3b12d4309174c5514a6fa8a23c573f434
anne <annewithanme@gmail.com>
2021-07-12 19:06:07 +0900

    210712 |feat: 메인 페이지

--- backend/shops/views.py
+++ backend/shops/views.py
@@ -1,15 +1,46 @@
-from django.http import response
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, action
+from random import sample
+from django.db.models          import Q, Value
+from rest_framework            import viewsets
+from rest_framework.decorators import action
+from rest_framework.response   import Response
 
-from django.views import View
+from .models      import Shop,Category
+from .serializers import ShopListSerializer, ShopDetailSerializer
 
-from shops import serializers, models
+class ShopListViewSet(viewsets.ModelViewSet):
+    def list(self,request):
+        latitude  = float(request.query_params.get('latitude'))
+        longitude = float(request.query_params.get('longitude'))
+        range_condition = (
+            Q(latitude__range = (latitude-0.01, latitude+0.01))&
+            Q(longitude__range= (longitude-0.015, longitude+0.015))
+        )
+        shops       = Shop.objects.filter(range_condition).order_by('-kakao_score')
+        result_list = []
+
+        if request.query_params.get('all') == 'True':
+            serializer = ShopListSerializer(shops,many=True)
+            result     = {'전체' : serializer.data}
+            result_list.append(result)
+                
+        if request.query_params.get('top') == 'True':
+            serializer = ShopListSerializer(shops[:10],many=True)
+            result     = {'오늘의 추천' : serializer.data}
+            result_list.append(result)
+
+        if request.query_params.get('category') == 'True':
+            categories = sample(range(1,Category.objects.all().count()+1),3)
+            for category_id in categories:
+                category            = Category.objects.get(id=category_id)
+                shops_category      = shops.filter(category=category)[:10]
+                serializer_category = ShopListSerializer(shops_category, many=True)
+                result              = {category.category_name + ' 맛집': serializer_category.data}
+                result_list.append(result)
+
+        return Response(result_list)
 
 class ShopDetailViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.ShopDetailSerializer
-    
+    serializer_class = ShopDetailSerializer
     def get_queryset(self):
-        queryset = models.Shop.objects.filter(id=self.kwargs['id'])
+        queryset = Shop.objects.filter(id=self.kwargs['id'])
         return queryset
