1ee09f8501474ee297a7312ca3cb6b97b7baad96
ttakku-park <ttakku.park@gmail.com>
2021-07-13 11:02:17 +0900

    210713 | feat : 네이버 로그인 url 추가

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
-from .views import KakaoLogInView, GoogleLoginView
+from .views import KakaoLogInView, GoogleLoginView, NaverLogInView
 router = DefaultRouter(trailing_slash=True)
 router.register(r"accounts", views.UserViewSet, basename="accounts")
 
 urlpatterns = [
     # views.py에서 정의한(def) 함수 연결 가능 (path)
     # path('send_sms/', views.send_sms),
+    path('naver-login/',NaverLogInView.as_view()),
     path('kakao-login/',KakaoLogInView.as_view()),
     path('google-login/',GoogleLoginView.as_view()),
     path('dislike-shop', views.dislikeshop),
