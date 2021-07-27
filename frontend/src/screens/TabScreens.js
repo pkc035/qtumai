07d7664b8bc0fca2ace8ff4790067c5cc081e03f
fghjjkl32 <fghjjkl32@naver.com>
2021-07-27 10:12:22 +0900

    210727 | fix : 병합 충돌 수정

--- frontend/src/screens/TabScreens.js
+++ frontend/src/screens/TabScreens.js
@@ -26,43 +26,13 @@ export const MainNavigator = () => {
 
 export const Main = ({ navigation }) => {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
     <MainContainer>
-<<<<<<< HEAD
-      <WebView source={{ uri: `${URI}/` }} />
-      <FloatingBackground>
-        <FloatingButton
-          position={{ bottom: 80, right: 40 }}
-          navigation={navigation}
-        />
-      </FloatingBackground>
-=======
-<<<<<<< HEAD
-
-      <WebView source={{ uri: 'http://192.168.0.69:3000/' }} />
-=======
-      <WebView source={{ uri: 'http://192.168.0.76:3000/' }} />
->>>>>>> 7a18f46 (Add: back up before pull)
-=======
-    <MainContainer onClick={() => console.log('good')}>
-      {/* <WebView source={{ uri: 'http://192.168.0.76:3000/' }} /> */}
-      <WebView source={{ uri: 'http://10.58.2.159:3000/' }} />
->>>>>>> 4f95984 (210722 | add picList in Detailpage)
-=======
-    <MainContainer>
-      <WebView
-        source={{ uri: 'http://192.168.0.76:3000/' }}
-        onPress={() => navigation.navigate('Who')}
-      />
+      <WebView source={{ uri: "http://192.168.0.76:3000/" }} />
       {/* <WebView source={{ uri: 'http://10.58.2.159:3000/' }} /> */}
->>>>>>> bc92698 (210725 | floating page layout, style)
-
       <FloatingButton
         position={{ bottom: 80, right: 40 }}
         navigation={navigation}
       />
->>>>>>> master
     </MainContainer>
   );
 };
@@ -74,15 +44,9 @@ const MainContainer = styled.View`
 export const Map = ({ navigation }) => {
   return (
     <Container>
-<<<<<<< HEAD
       <WebView source={{ uri: `${URI}/Map` }} />
       <Button title="Who" onPress={() => navigation.navigate("Who")} />
       <Button title="Where" onPress={() => navigation.navigate("Where")} />
-=======
-      <WebView source={{ uri: 'http://192.168.0.76:3000/Map' }} />
-      <Button title="Who" onPress={() => navigation.navigate('Who')} />
-      <Button title="Where" onPress={() => navigation.navigate('Where')} />
->>>>>>> master
     </Container>
   );
 };
@@ -90,7 +54,7 @@ export const Map = ({ navigation }) => {
 export const Like = () => {
   return (
     <Container>
-      <WebView source={{ uri: 'http://192.168.0.76:3000/likes' }} />
+      <WebView source={{ uri: "http://192.168.0.76:3000/likes" }} />
       {/* <WebView source={{ uri: 'http://10.58.2.159:3000/Detail' }} /> */}
     </Container>
   );
